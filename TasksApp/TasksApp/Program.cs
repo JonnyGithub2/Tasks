51e2f86f6a3474aa43ae142bd5671d06d2ae8ed1
JonnyGithub2 <115008517+JonnyGithub2@users.noreply.github.com>
2023-10-27 10:39:06 +0100

    added MenuItems, although none of it is in use yet, need to make more menuitems and connect them together

--- TasksApp/TasksApp/Program.cs
+++ TasksApp/TasksApp/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Tasks;
 
 Console.WriteLine("Welcome to my tasks management application");
+
+BoardManager boardManager = new BoardManager();
 Tasks.Task task1 = new Tasks.Task("this is my first task");
 Console.WriteLine(task1.ToString());
 Board board1 = new Board("this is my board");
@@ -10,3 +12,4 @@ Console.WriteLine(board1.listTasks());
 Tasks.Task task2 = new Tasks.Task("this is my second task");
 board1.addTask(task2);
 Console.WriteLine(board1.listTasks());
+boardManager.addBoard(board1);
