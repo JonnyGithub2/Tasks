52538c8bf3308f33d0517d3872bfbbfb0e45857d
JonnyGithub2 <115008517+JonnyGithub2@users.noreply.github.com>
2023-10-27 10:07:01 +0100

    Added Task.cs and Board.cs, will use these in future to keep track of tasks and boards

--- TasksApp/TasksApp/Program.cs
+++ TasksApp/TasksApp/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Tasks;
+
 Console.WriteLine("Welcome to my tasks management application");
+Tasks.Task task1 = new Tasks.Task("this is my first task");
+Console.WriteLine(task1.ToString());
+Board board1 = new Board("this is my board");
+board1.addTask(task1);
+Console.WriteLine(board1.ToString());
+Console.WriteLine(board1.listTasks());
+Tasks.Task task2 = new Tasks.Task("this is my second task");
+board1.addTask(task2);
+Console.WriteLine(board1.listTasks());
